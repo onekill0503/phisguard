eff8a4c2d892dabb8993f8f84c375d0a71d1b92a
onekill0503 <gamemasteraji123@gmail.com>
2025-02-07 17:50:38 +0700

    feat: integrate operator to autonome ai endpoint

--- avs/operator/index.ts
+++ avs/operator/index.ts
@@ -1,8 +1,11 @@
-import { ethers } from "ethers";
+import { ethers, encodeBytes32String } from "ethers";
 import * as dotenv from "dotenv";
+import axios from "axios";
 const fs = require('fs');
 const path = require('path');
 dotenv.config();
+const basicKey: string = process.env.AUTONOME_KEY!;
+const agentId: string = process.env.AUTONOME_AGENT_ID!;
 
 // Check if the process.env object is empty
 if (!Object.keys(process.env).length) {
@@ -16,28 +19,13 @@ const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
 let chainId = 31337;
 
 const avsDeploymentData = JSON.parse(fs.readFileSync(path.resolve(__dirname, `../contracts/deployments/hello-world/${chainId}.json`), 'utf8'));
-// Load core deployment data
-const coreDeploymentData = JSON.parse(fs.readFileSync(path.resolve(__dirname, `../contracts/deployments/core/${chainId}.json`), 'utf8'));
 
-
-const delegationManagerAddress = coreDeploymentData.addresses.delegation; // todo: reminder to fix the naming of this contract in the deployment file, change to delegationManager
-const avsDirectoryAddress = coreDeploymentData.addresses.avsDirectory;
 const helloWorldServiceManagerAddress = avsDeploymentData.addresses.helloWorldServiceManager;
-const ecdsaStakeRegistryAddress = avsDeploymentData.addresses.stakeRegistry;
 
 
-
-// Load ABIs
-const delegationManagerABI = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../abis/IDelegationManager.json'), 'utf8'));
-const ecdsaRegistryABI = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../abis/ECDSAStakeRegistry.json'), 'utf8'));
 const helloWorldServiceManagerABI = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../abis/HelloWorldServiceManager.json'), 'utf8'));
-const avsDirectoryABI = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../abis/IAVSDirectory.json'), 'utf8'));
 
-// Initialize contract objects from ABIs
-const delegationManager = new ethers.Contract(delegationManagerAddress, delegationManagerABI, wallet);
 const helloWorldServiceManager = new ethers.Contract(helloWorldServiceManagerAddress, helloWorldServiceManagerABI, wallet);
-const ecdsaRegistryContract = new ethers.Contract(ecdsaStakeRegistryAddress, ecdsaRegistryABI, wallet);
-const avsDirectory = new ethers.Contract(avsDirectoryAddress, avsDirectoryABI, wallet);
 
 interface Task {
     createdBlock: number,
@@ -55,7 +43,7 @@ const signAndRespondToTask = async (taskIndex: number, task: Task) => {
     console.log(`Signing and responding to task ${taskIndex}`);
 
     // Analisi By Ai (INCLUDE SIMULASI)
-    const response = await provider.send("debug_traceCall", [
+    const debugData = await provider.send("debug_traceCall", [
         {
           from: task.from,
           to: task.to,
@@ -64,7 +52,8 @@ const signAndRespondToTask = async (taskIndex: number, task: Task) => {
         ["trace"],
         "latest",
       ]);
-    console.log(response);
+    console.log(debugData);
+    const { isSafe, cause } = await getAiAnalysis(debugData);
 
 
     const operators = [await wallet.getAddress()];
@@ -78,71 +67,15 @@ const signAndRespondToTask = async (taskIndex: number, task: Task) => {
         [task.createdBlock, task.from, task.to, task.data, task.value],
         taskIndex,
         signedTask,
-        false,
-        '0x'
+        isSafe,
+        encodeBytes32String(cause)
     );
     await tx.wait();
     console.log(`Responded to task.`);
 };
 
-const registerOperator = async () => {
-    
-    // Registers as an Operator in EigenLayer.
-    try {
-        const tx1 = await delegationManager.registerAsOperator({
-            __deprecated_earningsReceiver: await wallet.address,
-            delegationApprover: "0x0000000000000000000000000000000000000000",
-            stakerOptOutWindowBlocks: 0
-        }, "");
-        await tx1.wait();
-        console.log("Operator registered to Core EigenLayer contracts");
-    } catch (error) {
-        console.error("Error in registering as operator:", error);
-    }
-    
-    const salt = ethers.hexlify(ethers.randomBytes(32));
-    const expiry = Math.floor(Date.now() / 1000) + 3600; // Example expiry, 1 hour from now
-
-    // Define the output structure
-    let operatorSignatureWithSaltAndExpiry = {
-        signature: "",
-        salt: salt,
-        expiry: expiry
-    };
-
-    // Calculate the digest hash, which is a unique value representing the operator, avs, unique value (salt) and expiration date.
-    const operatorDigestHash = await avsDirectory.calculateOperatorAVSRegistrationDigestHash(
-        wallet.address, 
-        await helloWorldServiceManager.getAddress(), 
-        salt, 
-        expiry
-    );
-    console.log(operatorDigestHash);
-    
-    // Sign the digest hash with the operator's private key
-    console.log("Signing digest hash with operator's private key");
-    const operatorSigningKey = new ethers.SigningKey(process.env.PRIVATE_KEY!);
-    const operatorSignedDigestHash = operatorSigningKey.sign(operatorDigestHash);
-
-    // Encode the signature in the required format
-    operatorSignatureWithSaltAndExpiry.signature = ethers.Signature.from(operatorSignedDigestHash).serialized;
-
-    console.log("Registering Operator to AVS Registry contract");
-
-    
-    // Register Operator to AVS
-    // Per release here: https://github.com/Layr-Labs/eigenlayer-middleware/blob/v0.2.1-mainnet-rewards/src/unaudited/ECDSAStakeRegistry.sol#L49
-    const tx2 = await ecdsaRegistryContract.registerOperatorWithSignature(
-        operatorSignatureWithSaltAndExpiry,
-        wallet.address
-    );
-    await tx2.wait();
-    console.log("Operator registered on AVS successfully");
-};
 
 const monitorNewTasks = async () => {
-    //console.log(`Creating new task "EigenWorld"`);
-    //await helloWorldServiceManager.createNewTask("EigenWorld");
 
     helloWorldServiceManager.on("NewTaskCreated", async (taskIndex: number, task: any) => {
         console.log(task)
@@ -158,6 +91,25 @@ const monitorNewTasks = async () => {
 
     console.log("Monitoring for new tasks...");
 };
+
+const getAiAnalysis = async (data: string ) => {
+    let isSafe : boolean = false;
+    let analysis : string = "Analysis not available";
+    try {
+        const message = `Hey, Can you analyze this result of debug_traceCall for me? ${data}, is this possibly phishing or not safe ?. response with json format { safe: boolean, cause: string }.`;
+        const response = await axios.post(`https://autonome.alt.technology/${agentId}/chat`, {
+            message: message,
+        } , {
+            headers: { Authorization: `Basic ${basicKey}` }
+        });
+        const responseJSON = JSON.parse(response.data.text);
+        isSafe = responseJSON.safe;
+        analysis = responseJSON.cause;
+    }catch(err: any){
+        console.log(`Error : ${err.message}`)
+    }
+    return { isSafe, cause: analysis };
+}
 
 const main = async () => {
     // await registerOperator();
